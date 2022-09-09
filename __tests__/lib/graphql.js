fe2d0d203aa5b041014a8c07976fb912166ddb32
DavidNeiOs <davidneios25@gmail.com>
2022-09-09 05:23:55 -0400

    added e2e test for getMyProfile

---
+++ __tests__/lib/graphql.js
@@ -0,0 +1,44 @@
+const http = require("axios");
+const { createTestScheduler } = require("jest");
+const _ = require("lodash");
+
+const throwOnErrors = ({ query, variables, errors }) => {
+  if (errors) {
+    const errorMessage = `
+      query: ${query.substr(0, 100)}
+
+      variables: ${JSON.stringify(variables, null, 2)}
+
+      error: ${JSON.stringify(errors, null, 2)} 
+    `;
+
+    throw new Error(errorMessage);
+  }
+};
+
+module.exports = async (url, query, variables = {}, auth) => {
+  const headers = {};
+  if (auth) {
+    headers.Authorization = auth;
+  }
+
+  try {
+    const resp = await http({
+      method: "post",
+      url,
+      headers,
+      data: {
+        query,
+        variables: JSON.stringify(variables),
+      },
+    });
+
+    const { data, errors } = resp.data;
+    throwOnErrors({ query, variables, errors });
+    return data;
+  } catch (err) {
+    const errors = _.get(err, "response.data.errors ");
+    createTestScheduler({ query, variables, errors });
+    throw err;
+  }
+};
