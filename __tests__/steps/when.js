7684607679c1c6476be7976349b0fc30d5e9810d
DavidNeiOs <davidneios25@gmail.com>
2022-09-13 19:23:13 -0400

    added fragments

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -4,7 +4,85 @@ const fs = require("fs");
 const velocityMapper = require("amplify-appsync-simulator/lib/velocity/value-mapper/mapper");
 const velocityTemplate = require("amplify-velocity-template");
 
-const GraphQL = require("../lib/graphql");
+const { GraphQL, registerFragment } = require("../lib/graphql");
+
+const myProfileFragment = `
+fragment myProfileFields on MyProfile {
+  id
+  name
+  screenName
+  imageUrl
+  backgroundImageUrl
+  bio
+  location
+  website
+  birthdate
+  createdAt    
+  followersCount
+  followingCount
+  tweetsCount
+  likesCounts
+}
+`;
+
+const otherProfileFragment = `
+fragment otherProfileFields on OtherProfile {
+  id
+  name
+  screenName
+  imageUrl
+  backgroundImageUrl
+  bio
+  location
+  website
+  birthdate
+  createdAt  
+  followersCount
+  followingCount
+  tweetsCount
+  likesCounts
+}
+`;
+
+const iProfileFragment = `
+fragment iProfileFields on IProfile {
+  ... on MyProfile {
+    ... myProfileFields
+  }
+  ... on OtherProfile {
+    ... otherProfileFields
+  }
+}
+`;
+
+const tweetFragment = `
+fragment tweetFields on Tweet {
+  id
+  profile {
+    ... iProfileFields
+  }
+  createdAt
+  text
+  replies
+  likes
+  retweets
+  liked  
+}
+`;
+
+const iTweetFragment = `
+fragment iTweetFields on ITweet {
+  ... on Tweet {
+    ... tweetFields
+  }
+}
+`;
+
+registerFragment("myProfileFields", myProfileFragment);
+registerFragment("otherProfileFields", otherProfileFragment);
+registerFragment("iProfileFields", iProfileFragment);
+registerFragment("tweetFields", tweetFragment);
+registerFragment("iTweetFields", iTweetFragment);
 
 const we_invoke_confirmUserSignup = async (username, name, email) => {
   const handler = require("../../functions/confirm-user-signup").handler;
@@ -97,20 +175,7 @@ const we_invoke_an_appsync_template = (templatePath, context) => {
 const a_user_calls_getMyProfile = async (user) => {
   const getMyProfile = `query MyQuery {
     getMyProfile {
-      createdAt
-      followersCount
-      followingCount
-      id
-      imageUrl
-      likesCounts
-      name
-      screenName
-      birthdate
-      bio
-      backgroundImageUrl
-      location
-      tweetsCount
-      website      
+      ... myProfileFields    
     }
   }`;
 
@@ -131,20 +196,7 @@ const a_user_calls_getMyProfile = async (user) => {
 const a_user_calls_editMyProfile = async (user, input) => {
   const getMyProfile = `mutation EditMyProfile($input: ProfileInput!) {
     editMyProfile(newProfile: $input) {
-      createdAt
-      followersCount
-      followingCount
-      id
-      imageUrl
-      likesCounts
-      name
-      screenName
-      birthdate
-      bio
-      backgroundImageUrl
-      location
-      tweetsCount
-      website      
+      ... myProfileFields
     }
   }`;
 
@@ -212,9 +264,7 @@ const a_user_calls_tweet = async (user, text) => {
       id
       createdAt
       profile {
-        id
-        name
-        screenName
+        ... iProfileFields
       }
       text
       replies
@@ -247,20 +297,7 @@ const a_user_calls_getTweets = async (user, userId, limit, nextToken) => {
     getTweets(userId: $userId, limit: $limit, nextToken: $nextToken){ 
       nextToken
       tweets {
-        id
-        createdAt
-        profile {
-          id
-          name
-          screenName
-        }
-        ... on Tweet {
-          text
-          replies
-          likes
-          retweets
-          liked       
-        }                
+        ... iTweetFields                
       }
     }
   }`;
@@ -290,20 +327,7 @@ const a_user_calls_getMyTimeline = async (user, limit, nextToken) => {
     getMyTimeline(limit: $limit, nextToken: $nextToken){ 
       nextToken
       tweets {
-        id
-        createdAt
-        profile {
-          id
-          name
-          screenName
-        }
-        ... on Tweet {
-          text
-          replies
-          likes
-          retweets  
-          liked        
-        }                
+        ... iTweetFields               
       }
     }
   }`;
